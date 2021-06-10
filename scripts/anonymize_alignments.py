14b254fda2f39d4db267e0e09c5b5daa054b0e1d
Austin Blodgett <ablodge@users.noreply.github.com>
2021-06-10 11:29:20 -0400

    add anonymization scripts

--- scripts/anonymize_alignments.py
+++ scripts/anonymize_alignments.py
@@ -5,108 +5,40 @@ from amr_utils.amr_readers import AMR_Reader
 
 
 def main():
-    amr_file_old = sys.argv[1]
-    amr_file_new = sys.argv[2]
-    output_file = sys.argv[3]
+    amr_file = sys.argv[1]
+    output_file = sys.argv[2]
 
     reader = AMR_Reader()
-    amrs_old = reader.load(amr_file_old)
-    amrs_new = reader.load(amr_file_new, remove_wiki=True)
+    amrs = reader.load(amr_file)
 
-
-    bad_node_map = {}
-    for amr1 in amrs_new:
-        amr2 = next(a for a in amrs_old if a.id == amr1.id)
-        for n in amr1.nodes:
-            amr1.nodes[n] = amr1.nodes[n].replace('"','')
-        for n in amr2.nodes:
-            amr2.nodes[n] = amr2.nodes[n].replace('"','')
-        bad_nodes = []
-        for n in amr1.nodes:
-            if n not in amr2.nodes or amr1.nodes[n] != amr2.nodes[n]:
-                bad_nodes.append(n)
-                continue
-            neighborhood = {f'{amr1.nodes[e[0]]} {e[1]} {amr1.nodes[e[2]]}' for e in amr1.edges if n in e}
-            neighborhood2 = {f'{amr2.nodes[e[0]]} {e[1]} {amr2.nodes[e[2]]}' for e in amr2.edges if n in e}
-            if neighborhood!=neighborhood2:
-                bad_nodes.append(n)
-        if bad_nodes:
-            bad_node_map[amr1.id] = {}
-            for n in amr1.nodes:
-                if n in bad_nodes:
-                    new_n = [n2 for n2 in amr2.nodes
-                             if amr2.nodes[n2]==amr1.nodes[n]
-                             and not (n2 in amr1.nodes and amr1.nodes[n2]==amr1.nodes[n])]
-                    neighborhood = {f'{amr1.nodes[e[0]]} {e[1]} {amr1.nodes[e[2]]}' for e in amr1.edges if n in e}
-                    if len(new_n)>1:
-                        neighborhood2 = {n2:[f'{amr2.nodes[e[0]]} {e[1]} {amr2.nodes[e[2]]}' for e in amr2.edges if n2 in e] for n2 in new_n}
-                        new_n = [n2 for n2 in new_n if neighborhood==set(neighborhood2[n2])]
-                    if len(new_n)==1:
-                        bad_node_map[amr1.id][new_n[0]] = n
-                    else:
-                        raise Exception('Bad node match', amr1.id, n)
-                else:
-                    bad_node_map[amr1.id][n]=n
-    for amr1 in amrs_new:
-        amr2 = next(a for a in amrs_old if a.id == amr1.id)
-        for n2 in amr2.nodes:
-            n = bad_node_map[amr1.id][n2] if amr1.id in bad_node_map else n2
-            if amr1.nodes[n] != amr2.nodes[n2]:
-                raise Exception('Bad node match', amr1.id, n)
-        for e in amr2.edges:
-            s2,r,t2 = e
-            s = bad_node_map[amr1.id][s2] if amr1.id in bad_node_map else s2
-            t = bad_node_map[amr1.id][t2] if amr1.id in bad_node_map else t2
-            if (s,r,t) not in amr1.edges:
-                raise Exception('Bad edge match', amr1.id, e, amr1.nodes[s],r.amr1.nodes[t])
-
-    print('Node id fixes:',len(bad_node_map), ' '.join(bad_node_map.keys()))
     # subgraphs
-    align_file = amr_file_old.replace('.txt','')+'.subgraph_alignments.gold.json'
+    align_file = amr_file.replace('.txt','')+'.subgraph_alignments.json'
     sub_aligns = reader.load_alignments_from_json(align_file)
-    for amr in amrs_new:
-        for align in sub_aligns[amr.id]:
-            if amr.id in bad_node_map:
-                align.nodes = [bad_node_map[amr.id][n] for n in align.nodes]
-        for align in sub_aligns[amr.id]:
-            if len(align.nodes)>1:
-                for s,r,t in amr.edges:
-                    if s in align.nodes and t in align.nodes:
-                        align.edges.append((s,r,t))
+    for amr in amrs:
         sub_aligns[amr.id] = [a for a in sub_aligns[amr.id] if a.nodes]
-    align_file = output_file.replace('.txt', '') + '.subgraph_alignments.gold.json'
+    align_file = output_file.replace('.txt', '') + '.subgraph_alignments.json'
     print('Writing subgraph alignments to:', align_file)
-    write_to_json(align_file, sub_aligns, anonymize=True, amrs=amrs_new)
+    write_to_json(align_file, sub_aligns, anonymize=True, amrs=amrs)
 
     # relations
-    align_file = amr_file_old.replace('.txt', '') + '.relation_alignments.gold.json'
+    align_file = amr_file.replace('.txt', '') + '.relation_alignments.json'
     rel_aligns = reader.load_alignments_from_json(align_file)
-    for amr in amrs_new:
-        for align in rel_aligns[amr.id]:
-            if amr.id in bad_node_map:
-                align.edges = [(bad_node_map[amr.id][s],r,bad_node_map[amr.id][t]) for s,r,t in align.edges]
-        for align in rel_aligns[amr.id]:
-            sub_align = amr.get_alignment(sub_aligns, token_id=align.tokens[0])
-            if sub_align.nodes:
-                align.edges = [e for e in align.edges if not (e[0] in sub_align.nodes and e[-1] in sub_align.nodes)]
+    for amr in amrs:
         rel_aligns[amr.id] = [a for a in rel_aligns[amr.id] if a.edges]
-    align_file = output_file.replace('.txt', '') + '.relation_alignments.gold.json'
+    align_file = output_file.replace('.txt', '') + '.relation_alignments.json'
     print('Writing relation alignments to:', align_file)
-    write_to_json(align_file, rel_aligns, anonymize=True, amrs=amrs_new)
+    write_to_json(align_file, rel_aligns, anonymize=True, amrs=amrs)
 
     # reentrancies
-    align_file = amr_file_old.replace('.txt', '') + '.reentrancy_alignments.gold.json'
+    align_file = amr_file.replace('.txt', '') + '.reentrancy_alignments.json'
     reent_aligns = reader.load_alignments_from_json(align_file)
-    for amr in amrs_new:
+    for amr in amrs:
         reent_aligns[amr.id] = [a for a in reent_aligns[amr.id] if a.edges]
-        for align in reent_aligns[amr.id]:
-            if amr.id in bad_node_map:
-                align.edges = [(bad_node_map[amr.id][s],r,bad_node_map[amr.id][t]) for s,r,t in align.edges]
-    align_file = output_file.replace('.txt', '') + '.reentrancy_alignments.gold.json'
+    align_file = output_file.replace('.txt', '') + '.reentrancy_alignments.json'
     print('Writing reentrancy alignments to:', align_file)
-    write_to_json(align_file, reent_aligns, anonymize=True, amrs=amrs_new)
+    write_to_json(align_file, reent_aligns, anonymize=True, amrs=amrs)
 
-    for amr in amrs_new:
+    for amr in amrs:
         for n in amr.nodes:
             n_aligned = [a for a in sub_aligns[amr.id] if n in a.nodes]
             if len(n_aligned)!=1:
---
+++ unanonymize_alignments.py
@@ -0,0 +1,27 @@
+import os
+
+from amr_utils.alignments import load_from_json, write_to_json
+from amr_utils.amr_readers import AMR_Reader
+
+
+def main():
+    dir = 'data-release/alignments'
+
+    reader = AMR_Reader()
+    dev_amrs = reader.load('data-release/amrs/leamr_dev.txt')
+    test_amrs = reader.load('data-release/amrs/leamr_test.txt')
+    all_amrs = reader.load('data-release/amrs/ldc+little_prince.txt')
+
+    amr_map = {'leamr_dev':dev_amrs, 'leamr_test':test_amrs, 'ldc+little_prince':all_amrs}
+    for filename in os.listdir(dir):
+        file = os.path.join(dir, filename)
+        if file.endswith('alignments.json') or file.endswith('alignments.gold.json'):
+            for k in amr_map:
+                if filename.startswith(k):
+                    amrs = amr_map[k]
+                    aligns = load_from_json(file, amrs, unanonymize=True)
+                    write_to_json(file, aligns, amrs=amrs, anonymize=False)
+                    break
+
+if __name__=='__main__':
+    main()
